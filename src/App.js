fc7c4cae1e9430a6ee7e34ef747a95108a9fb0e8
Ansifnk <ansifansif786@gmail.com>
2022-04-02 10:22:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -6,6 +5,7 @@ import WorkSpace from './pages/workSpace/WorkSpace';
 
 function App() {
   return (
+  
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
